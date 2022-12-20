2716d933ae7d77ce74ebe6cdb79171a1c50c4c69
Vyacheslav Filippov <vyacheslav.filippov@iridato.com>
2022-12-20 17:13:43 +0300

    Training JS #7: if..else and ternary operator

--- 6.js
+++ 6.js
@@ -2,6 +2,6 @@ function saleHotdogs(n){
     var sum = 0
     if (n < 5) sum = n * 100
     else if (n >= 5 && n < 10) sum = n * 95
-    else sum = n * 90
+    else sum = n * 80
     return sum
   }
