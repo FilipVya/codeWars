a042741d089c16e5cdc946c26927b1552dc4e396
FilipVya <vyacheslav.filippov@iridato.com>
2023-01-10 13:29:47 +0300

    Keep Hydrated!

--- 28.js
+++ 28.js
@@ -1,13 +1,26 @@
 function solution(str, ending){
     let aLen = str.length;
     let bLen = ending.length;
+    let arr1 = [];
+    let arr2 = [];
+    if(bLen === 0){
+      return true
+    } else {
+    for(j = aLen - bLen; j < aLen; j++){
+       arr1.push(str[j]);
+           }
     for(i = 0; i < bLen; i++){
-      for(j = aLen - bLen; j < aLen; j++){
-        if(str[j] === ending[i]){
-          return true
-        } else {
-          return false
-        }
-      }
+        arr2.push(ending[i])
+    } 
+    if(arr1 == arr2) {
+        return true
+    } else {
+        return false
+    }
     }
   }
+    console.log(solution('abc', 'bc'))
+function equal(a, b){
+   return(a === b) 
+}
+console.log(equal("[4,3]", "[4,2]"))
